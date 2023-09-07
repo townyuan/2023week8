15a06fe32c0d2ba695c6123123236b41e1870248
meifang <>
2023-09-07 16:43:18 +0800

    update index

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
-import './assets/scss/all.scss';
 
-console.log("Hello world!");
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+import AOS from 'aos';
+
+import './assets/scss/all.scss';
+import './node_modules/bootstrap/dist/js/bootstrap.min.js';
+
+
+const swiperAlpha = new Swiper('.sw1', {
+  autoplay: {
+    delay: 1000,
+  },
+  breakpoints: {
+    768: {
+      autoplay: {
+        enabled: false,
+      }
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiper1 = new Swiper('.sw2', {
+  slidesPerView: "auto",
+  slidesPerView: 1,
+  spaceBetween: 10,
+  noSwiping: true,
+  // Responsive breakpoints
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 24
+    },
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    }
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true, //拖曳
+  },
+  mousewheel: true, // 滑鼠滾動
+});
+
+
+//AOS
+AOS.init();
